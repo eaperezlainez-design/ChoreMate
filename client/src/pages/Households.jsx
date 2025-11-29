9cfd83a6d39bf1660d855044e91558f7987f3d07
eaperezlainez-design <eaperezlainez@csuchico.edu>
2025-11-28 18:48:25 -0800

    Add Households.jsx

---
+++ client/src/pages/Households.jsx
@@ -0,0 +1,20 @@
+export default function Households() {
+  return (
+    <div>
+      <h2>Households</h2>
+
+      <p>
+        View and manage your shared households. Each household contains members
+        and a list of chores.
+      </p>
+
+      <h3>Planned Sections</h3>
+      <ul>
+        <li>List of households</li>
+        <li>Create new household form</li>
+        <li>View household details</li>
+        <li>List of members in each household</li>
+      </ul>
+    </div>
+  );
+}
