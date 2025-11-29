3a513c685cf96c165f186fd63668cfe15694010d
eaperezlainez-design <eaperezlainez@csuchico.edu>
2025-11-28 18:50:36 -0800

    Add Assignments.jsx

---
+++ client/src/pages/Assignments.jsx
@@ -0,0 +1,20 @@
+export default function Assignments() {
+  return (
+    <div>
+      <h2>Assignments</h2>
+
+      <p>
+        View and manage chore assignments. Assign tasks to roommates, track due
+        dates, and monitor completion status.
+      </p>
+
+      <h3>Planned Sections</h3>
+      <ul>
+        <li>List of all assignments (pending / completed)</li>
+        <li>Assign chore form</li>
+        <li>Mark assignment as completed</li>
+        <li>Filter by roommate or due date</li>
+      </ul>
+    </div>
+  );
+}
