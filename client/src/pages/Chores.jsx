ea40a71eec2fb28bf27846aa05b54dae7ea4dc51
eaperezlainez-design <eaperezlainez@csuchico.edu>
2025-11-28 18:49:24 -0800

    Add Chores.jsx

---
+++ client/src/pages/Chores.jsx
@@ -0,0 +1,20 @@
+export default function Chores() {
+  return (
+    <div>
+      <h2>Chores</h2>
+
+      <p>
+        View and manage chores for your household. Chores have descriptions,
+        frequencies, and can be assigned to members.
+      </p>
+
+      <h3>Planned Sections</h3>
+      <ul>
+        <li>List of chores (by household)</li>
+        <li>Create new chore form</li>
+        <li>Edit chore details</li>
+        <li>Filter chores by frequency (daily/weekly/monthly)</li>
+      </ul>
+    </div>
+  );
+}
